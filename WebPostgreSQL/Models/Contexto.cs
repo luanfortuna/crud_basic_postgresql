73be5859d29a543663b18c284022c70d5d24ec44
Luan Fortuna <luan395@gmail.com>
2023-01-16 08:52:57 -0300

    primeiro commit

---
+++ WebPostgreSQL/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebPostgreSQL.Models
+{
+    public class Contexto : DbContext
+
+    {
+
+        public Contexto(DbContextOptions<Contexto> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
